4d770b657f9020aa6cf23536e4b04b4127c069a5
Your Name <coughlin.nicholas@gmail.com>
2020-05-02 11:14:14 -0300

    Working State update for coordinates

--- src/index.js
+++ src/index.js
@@ -2,12 +2,37 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-  render() {
+  // javascript class that initializes state
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null, long: null };
+
     window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
+      (position) => {
+        // we called setstate!
+        this.setState({
+          lat: position.coords.latitude,
+          long: position.coords.longitude,
+        });
+      },
       (err) => console.log(err)
     );
-    return <div> Latitude: </div>;
+  }
+
+  // react requires us to define render
+  render() {
+    return (
+      <div className="ui container">
+        <div className="ui card">
+          <div className="content">
+            <div className="header">Current Position:</div>
+            <div className="description">Latitude: {this.state.lat}</div>
+            <div className="description">Longitude: {this.state.long}</div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
