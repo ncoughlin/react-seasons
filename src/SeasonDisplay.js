61b578c2aafd466eab17783a7fbb649a9a610791
Your Name <coughlin.nicholas@gmail.com>
2020-05-04 11:22:52 -0300

    now displaying icon depending on the season

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,8 +1,18 @@
 import React from "react";
 
+// depending on the month and latitude determine if it is summer or winter
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat < 0 ? "summer" : "winter";
+  }
+};
+
 const SeasonDisplay = (props) => {
-  console.log(props.lat);
-  return <div> Season Display </div>;
+  const season = getSeason(props.lat, new Date().getMonth());
+  const seasonIcon = season === 'winter' ? '❄️':'🌞';
+  return <div> Season:{seasonIcon} </div>;
 };
 
 export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -32,13 +32,11 @@ class App extends React.Component {
       // if there is no error message and there IS position data
     } else if (!this.state.errorMessage && this.state.lat) {
       return (
-        
-          <LatLongCard>
-            <div className="description">Latitude: {this.state.lat}</div>
-            <div className="description">Longitude: {this.state.long}</div>
-            <SeasonDisplay lat={this.state.lat} />
-          </LatLongCard>
-
+        <LatLongCard>
+          <div className="description">Latitude: {this.state.lat}</div>
+          <div className="description">Longitude: {this.state.long}</div>
+          <SeasonDisplay lat={this.state.lat} />
+        </LatLongCard>
       );
       // if no error message and no position data
     } else {
